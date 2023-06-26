71e16727f50aa302004216a106bc759cf79b209f
Miras <miamib531@gmail.com>
2023-06-26 18:00:19 +0600

    feat: add print working dir

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,6 +1,12 @@
+let currentDirectory
+const printCurrentDirectory = () => {
+  console.log(`You are currently in ${currentDirectory}`)
+}
+
 export const startFileManager = () => {
   const args = process.argv.slice(2)
   const username = args.find(arg => arg.startsWith('--username=')).split('=')[1]
-
+  currentDirectory = process.cwd()
   console.log(`Welcome to the File Manager, ${username}!`)
+  printCurrentDirectory()
 }
