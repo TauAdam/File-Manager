df1c05a985ae8a9454036a89a0fde89604053d02
Miras <miamib531@gmail.com>
2023-06-26 20:20:39 +0600

    feat: read process.stdin

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,3 +1,6 @@
+import os from 'os'
+import { readInput } from './input.js'
+
 let currentDirectory
 const printCurrentDirectory = () => {
   console.log(`You are currently in ${currentDirectory}`)
@@ -6,7 +9,8 @@ const printCurrentDirectory = () => {
 export const startFileManager = () => {
   const args = process.argv.slice(2)
   const username = args.find(arg => arg.startsWith('--username=')).split('=')[1]
-  currentDirectory = process.cwd()
+  currentDirectory = os.homedir()
   console.log(`Welcome to the File Manager, ${username}!`)
   printCurrentDirectory()
+  readInput(username)
 }
---
+++ src/input.js
@@ -0,0 +1,40 @@
+import { createInterface } from 'readline'
+
+export const readInput = username => {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  rl.prompt()
+
+  rl.on('line', input => {
+    if (input === '.exit') {
+      rl.close()
+    } else {
+      handleInput(input.trim())
+      rl.prompt()
+    }
+  })
+
+  rl.on('close', () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+    process.exit(0)
+  })
+}
+
+const handleInput = input => {
+  const [command, ...args] = input.split(' ')
+
+  switch (command) {
+    case 'up':
+      goUp()
+      break
+    case 'cd':
+      goToDirectory(args[0])
+      break
+    case 'ls':
+      listContents()
+      break
+  }
+}
